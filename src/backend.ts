67d3cd3f3540dd3af355ce1bd6f855399a7ee42a
nandobfer <nandobfer@yahoo.com.br>
2024-02-20 15:22:43 -0300

    update: url

--- src/backend.ts
+++ src/backend.ts
@@ -1,2 +1,2 @@
-// export const url = "s://app.agencyboz.com:4111"
-export const url = "://localhost:4111"
+export const url = "s://app.agencyboz.com:4111"
+// export const url = "://localhost:4111"
