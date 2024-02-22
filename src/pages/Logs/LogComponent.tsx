72967bbbc3b045a4c5b535d37e2aca6ff33f68ca
Tainara Novaes <tainarasn.sba@gmail.com>
2024-02-21 22:25:31 -0300

    feat: styling logs card

--- src/pages/Logs/LogComponent.tsx
+++ src/pages/Logs/LogComponent.tsx
@@ -8,9 +8,26 @@ interface LogComponentProps {
 
 export const LogComponent: React.FC<LogComponentProps> = ({ log }) => {
     return (
-        <Box sx={{ alignItems: "center", gap: 3, justifyContent: "space-between" }}>
+        <Box
+            sx={{
+                alignItems: "center",
+                gap: 4,
+                padding: 2.5,
+                borderRadius: 3,
+                justifyContent: "space-between",
+                boxShadow: "rgba(189, 147, 135, 0.2) 0px 4px 16px",
+            }}
+        >
             <Box>{log.text}</Box>
-            <Box>{new Date(Number(log.timestamp)).toLocaleDateString("pt-br", { hour: "2-digit", minute: "2-digit", second: "2-digit" })}</Box>
+            <Box>
+                <p style={{ fontSize: "0.8vw" }}>
+                    {new Date(Number(log.timestamp)).toLocaleDateString("pt-br", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        second: "2-digit",
+                    })}
+                </p>
+            </Box>
         </Box>
     )
 }
--- src/pages/Logs/Logs.tsx
+++ src/pages/Logs/Logs.tsx
@@ -27,9 +27,9 @@ export const Logs: React.FC<LogsProps> = ({}) => {
     return (
         <Box sx={default_content_wrapper_style}>
             <ListHeader title="Logs" Icon={TbUsers} />
-            <Paper elevation={0} sx={default_content_list_style}>
+            <Paper elevation={0} sx={{ ...default_content_list_style, boxShadow: 0 }}>
                 <SearchField original_list={logs.list} setList={setList} search_key={"text"} />
-                <Box sx={{ flexDirection: "column", padding: 3, gap: 1 }}>
+                <Box sx={{ flexDirection: "column", paddingTop: 3, pl: 1, pr: 1, gap: 2 }}>
                     {list
                         .sort((a, b) => b.id - a.id)
                         .map((log) => (
